25f6dfe2f09827ff81f3f44d2a3cb7b2c8e834af
HappyTobi <happytobi@tscoding.de>
2020-05-12 16:05:54 +0200

    Initial setup

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
+is-executable dockutil || brew install dockutil
+
 dockutil --no-restart --remove all
+
+dockutil --no-restart --add "/Applications/Safari.app"
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Mail.app"
+dockutil --no-restart --add "/Applications/Microsoft Edge.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Utilities/Terminal.app"
-dockutil --no-restart --add "/Applications/System Preferences.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
+dockutil --no-restart --add "/Applications/Microsoft Teams.app"
+dockutil --no-restart --add "/Applications/Slack.app"
+dockutil --no-restart --add "/Applications/iTerm.app"
 
 killall Dock
--- runcom/.bash_profile
+++ runcom/.bash_profile
@@ -23,15 +23,9 @@ PATH="$DOTFILES_DIR/bin:$PATH"
 
 # Source the dotfiles (order matters)
 
-for DOTFILE in "$DOTFILES_DIR"/system/.{function,function_*,path,env,alias,grep,prompt,nvm,completion,custom}; do
+for DOTFILE in "$DOTFILES_DIR"/system/.{alias,env,grep,macros,path,prompt}; do
   [ -f "$DOTFILE" ] && . "$DOTFILE"
 done
-
-if is-macos; then
-  for DOTFILE in "$DOTFILES_DIR"/system/.{env,alias,function,path}.macos; do
-    [ -f "$DOTFILE" ] && . "$DOTFILE"
-  done
-fi
 
 # Set LSCOLORS
 
