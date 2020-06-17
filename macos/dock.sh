947a3ec637aba719192795059123edf9b29ed144
Timo Knapp <timo_knapp@hotmail.com>
2020-06-17 16:27:47 +0200

    [UPDATE] modify apps in dock

--- macos/dock.sh
+++ macos/dock.sh
@@ -4,16 +4,18 @@ is-executable dockutil || brew install dockutil
 
 dockutil --no-restart --remove all
 
-dockutil --no-restart --add "/Applications/Safari.app"
+#dockutil --no-restart --add "/Applications/Safari.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
-dockutil --no-restart --add "/Applications/Google Chrome.app"
-dockutil --no-restart --add "/Applications/Microsoft Edge.app"
-#dockutil --no-restart --add "/System/Applications/Calendar.app"
-dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
+#dockutil --no-restart --add "/Applications/Google Chrome.app"
+#dockutil --no-restart --add "/Applications/Microsoft Edge.app"
+dockutil --no-restart --add "/System/Applications/Calendar.app"
+dockutil --no-restart --add "/System/Applications/Mail.app"
+#dockutil --no-restart --add "/Applications/Microsoft Outlook.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/Spotify.app"
+#dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Microsoft Teams.app"
-dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/Alacritty.app"
+#dockutil --no-restart --add "/Applications/Slack.app"
+#dockutil --no-restart --add "/Applications/Alacritty.app"
+dockutil --no-restart --add "/Applications/iTerm2.app"
 
 killall Dock
