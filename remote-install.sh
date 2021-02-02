ff75119b9c7f37429c34ebf72daa2f4be210b94a
Timo Knapp <mail@timoknapp.com>
2021-02-02 22:54:32 +0100

    fix: change path to git repo

--- remote-install.sh
+++ remote-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SOURCE="https://github.com/happytobi/dotfiles"
+SOURCE="https://github.com/timoknapp/dotfiles"
 TARBALL="$SOURCE/tarball/master"
 TARGET="$HOME/.dotfiles"
 TAR_CMD="tar -xzv -C "$TARGET" --strip-components=1 --exclude='{.gitignore}'"
