f4a1c909be34bbb89f64879be4ff1d03fba258fd
timoknapp <mail@timoknapp.com>
2022-12-12 18:46:21 +0100

    feat(amphetamine): set default duration to 4 hours

--- macos/amphetamine.sh
+++ macos/amphetamine.sh
@@ -4,6 +4,7 @@ echo "Setting Amphetamine preferences..."
 
 defaults write com.if.Amphetamine "Start Session At Launch" -int 1
 defaults write com.if.Amphetamine "Enable Session State Sound" -int 0
+defaults write com.if.Amphetamine "Default Duration" -int 17
 
 killall Amphetamine
 open /Applications/Amphetamine.app
