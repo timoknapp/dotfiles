78a09d90c802891fd20d7e79aba9f07a95704d81
timoknapp <mail@timoknapp.com>
2022-12-12 16:27:57 +0100

    feat: disable sound for amphetamine

--- macos/amphetamine.sh
+++ macos/amphetamine.sh
@@ -3,6 +3,7 @@
 echo "Setting Amphetamine preferences..."
 
 defaults write com.if.Amphetamine "Start Session At Launch" -int 1
+defaults write com.if.Amphetamine "Enable Session State Sound" -int 0
 
 killall Amphetamine
 open /Applications/Amphetamine.app
